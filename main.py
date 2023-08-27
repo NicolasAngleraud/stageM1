c0a8d9970764596722962ce5564b55d270201938
Nicolas Angleraud <46026463+NicolasAngleraud@users.noreply.github.com>
2023-08-27 22:09:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import pickle
-import random
-from collections import defaultdict
 import argparse
 import torch
 import wiktionary as wi
 import classifier as clf
-import data
-from sklearn.model_selection import train_test_split
 
 
 # supersenses acknowleged
@@ -117,11 +112,6 @@ if __name__ == '__main__':
         wiki_parser.parse_file()
 
     if args.main_mode == "classify":
-        train_examples, dev_examples, test_examples = clf.encoded_examples_split(args.definition_begins_with_lemma,
-                                                                                 train=args.train_file,
-                                                                                 dev=args.dev_file,
-                                                                                 test=args.test_file)
-
 
         # DEVICE setup
         device_id = args.device_id
@@ -130,11 +120,19 @@ if __name__ == '__main__':
 
         # Classification program
         with open("logs_file.txt", 'w', encoding="utf-8") as file:
-            for i in range(3):
+            for i in range(1):
                 for def_mode in ['definition', 'definition_with_lemma', 'definition_with_labels', 'definition_with_lemma_and_labels']:
-                    for lr in [0.005]:
-                    # for lr in [0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]:
-                        for patience in [5]:
+                    train_examples, dev_examples, test_examples = clf.encoded_examples_split(def_mode,
+                                                                                             train=args.train_file,
+                                                                                             dev=args.dev_file,
+                                                                                             test=args.test_file)
+
+                    for lr in [0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]:
+                        for patience in [10]:
+
+                            print("")
+                            print(f"run {i+1} : lr = {lr}; mode = {def_mode}")
+                            print("")
 
                             hypersense_dist = {hypersense: 0 for hypersense in HYPERSENSES}
                             hypersense_correct = {hypersense: 0 for hypersense in HYPERSENSES}
